a8ad072c416cb6d43e1ec47bc28a817f57aec08d
Jimin Ha <smjmjh26@naver.com>
2023-04-02 18:46:22 +0900

    Animal Selection App Using Tap Hosts

---
+++ app/src/main/java/com/example/project6_3/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.project6_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+@SuppressWarnings("deprecation")
+public class MainActivity extends TabActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TabHost tabHost=getTabHost();
+        TabHost.TabSpec tabSpecdog=tabHost.newTabSpec("dog").setIndicator("강아지");
+        tabSpecdog.setContent(R.id.DOG);
+        tabHost.addTab(tabSpecdog);
+        TabHost.TabSpec tabSpeccat=tabHost.newTabSpec("cat").setIndicator("고양이");
+        tabSpeccat.setContent(R.id.CAT);
+        tabHost.addTab(tabSpeccat);
+        TabHost.TabSpec tabSpecrab=tabHost.newTabSpec("rabbit").setIndicator("토끼");
+        tabSpecrab.setContent(R.id.RABBIT);
+        tabHost.addTab(tabSpecrab);
+        TabHost.TabSpec tabSpeclion=tabHost.newTabSpec("horse").setIndicator("말");
+        tabSpeclion.setContent(R.id.HORSE);
+        tabHost.addTab(tabSpeclion);
+        tabHost.setCurrentTab(0);
+    }
+}
